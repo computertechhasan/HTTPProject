ad28c3a7a52c797d26f1ed5b71e7c174f1a63c18
computertechhasan <33268183+computertechhasan@users.noreply.github.com>
2018-03-27 16:51:18 -0400

    Finished

--- HTTPClient.py
+++ HTTPClient.py
@@ -1,3 +1,6 @@
+##Hasan Intisar
+## hi6
+## Section 002
 import sys
 from socket import *
 import struct
@@ -17,21 +20,16 @@ server = requestInfo[0]
 port = int(requestInfo[1])
 fileName = requestInfo[2]
 count = 1000000
-
-httpRequestContainer = "PUTHTTPREQUESTHERE"
-data = "I'm alive."
+##Normal Request
+##httpRequestContainer = "GET " + fileName + " HTTP/1.1\r\n Host: " + server + ":" + str(port) + "\r\n\r\n" 
+##IfModifiedRequest
+httpRequestContainer = "GET " + fileName + " HTTP/1.1\r\nHost: " + server + ":" + str(port) + "\r\nIf-Modified-Since: Tue, 27 Mar 2018 16:06:02\r\n" 
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 print("Attempting to connect to " + server + " on port " + str(port) + ".")
 clientSocket.connect((server,port))
-print("Sending data to server")
-clientSocket.send(data.encode())
+print("The request being sent to the server is:\r\n" + httpRequestContainer)
+clientSocket.send(httpRequestContainer.encode())
 dataEcho = clientSocket.recv(count)
-print("The server responded " + dataEcho.decode())
+print(dataEcho.decode())
 clientSocket.close()
-
-
-
-
-
-
--- HTTPServer.py
+++ HTTPServer.py
@@ -1,12 +1,15 @@
+##Hasan Intisar
+## hi6
+## Section 002
 import sys
 from socket import *
 import struct
+import datetime, time
+import os.path
 ##Declaring server values
 serverIP = "127.0.0.1"
 serverPort = 12000
 dataLen = 1000000
-
-
 ##Creating the server socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((serverIP, serverPort))
@@ -18,8 +21,40 @@ while True:
     connectionSocket, address = serverSocket.accept()
     print("Socket created!")
     data = connectionSocket.recv(dataLen).decode()
-    print("The client sent " + data)
-    data = "Hi I'm alive"
-    connectionSocket.send(data.encode())
+    requestHolder = data.split()
+    print("The client sent the following request: " + data)
+    ifModifiedSinceDateString = ""
+    ##Parse for key info
+    for index in range(0,len(requestHolder)):
+        if requestHolder[index] == "GET":
+            fileName = requestHolder[index+1]
+        if requestHolder[index] == "If-Modified-Since:":          
+            ifModifiedSinceDateString = requestHolder[index+1] + requestHolder[index+2] + " " + requestHolder[index+3] + " " + requestHolder[index+4] + " " + requestHolder[index+5]
+    fileFailure = 0
+    currTime = datetime.datetime.now()
+    httpHeaderDate = currTime.strftime("%a, %d %b %Y %H:%M:%S %Z\r\n")
+    ##Handle file not found
+    try:
+        with open(fileName) as webFile:
+            fileContents = webFile.read()
+    except:
+        fileFailure = 1
+    if fileFailure == 0:
+        fileLastModifiedSecs = os.path.getmtime(fileName)
+        if ifModifiedSinceDateString != "":
+            ifModifiedSinceDate = datetime.datetime.strptime(ifModifiedSinceDateString,"%a,%d %b %Y %H:%M:%S")
+            ifModifiedSinceDateSeconds = ifModifiedSinceDate.timestamp()
+        if ifModifiedSinceDateString != "":
+            if fileLastModifiedSecs > ifModifiedSinceDateSeconds:
+                responseHeader = "HTTP/1.1 200 OK\r\nDate: " + str(httpHeaderDate) + "Content Length: " + str(len(fileContents)) + "\r\n" + "Content-Type: text/html; charset=UTF-8\r\n"
+                responseMessage = responseHeader + fileContents
+            else:
+                responseMessage = "HTTP/1.1 304 Not Modified\r\nDate: " + str(httpHeaderDate) +"\r\n"
+        else:
+            responseHeader = "HTTP/1.1 200 OK\r\nDate: " + str(httpHeaderDate) + "Content Length: " + str(len(fileContents)) + "\r\n" + "Content-Type: text/html; charset=UTF-8\r\n"
+            responseMessage = responseHeader + fileContents
+    else:
+        responseMessage = "HTTP/1.1 404 Not Found\r\nDate: " + str(httpHeaderDate) + "\r\n"
+    connectionSocket.send(responseMessage.encode())
     connectionSocket.close()
 

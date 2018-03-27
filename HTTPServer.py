bbe36048361f38784ee8a6cc96244ee2f2da0fdf
computertechhasan <33268183+computertechhasan@users.noreply.github.com>
2018-03-26 22:02:58 -0400

    HTML is here

--- HTTPServer.py
+++ HTTPServer.py
@@ -1,15 +1,19 @@
 import sys
 from socket import *
 import struct
-
+##Declaring server values
 serverIP = "127.0.0.1"
 serverPort = 12000
 dataLen = 1000000
 
+
+##Creating the server socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((serverIP, serverPort))
 serverSocket.listen(1)
 print("The server is listening on port: " + str(serverPort))
+
+##Handling requests
 while True:
     connectionSocket, address = serverSocket.accept()
     print("Socket created!")
